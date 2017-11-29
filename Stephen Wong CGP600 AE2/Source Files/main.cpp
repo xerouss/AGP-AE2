8846b384e4961b700be88db912af8e520bd70a23
Stephen Wong <stephen.wong96@gmail.com>
2017-11-29 10:28:38 +0000

    Base project set up

---
+++ Stephen Wong CGP600 AE2/Source Files/main.cpp
@@ -0,0 +1,14 @@
+// *********************************************************
+//	Name:			Stephen Wong
+//	File:			main.cpp
+//	Last Updated:	29/11/2017
+//	Project:		CGP600 AE2
+// *********************************************************
+
+#pragma region Includes
+
+#pragma endregion
+
+#pragma region Functions
+
+#pragma endregion
